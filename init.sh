34d8e040a5ff264993ae0ab484b8d698969f4cb0
weikunkun <kongwiki@163.com>
2021-11-28 20:06:02 +0800

    bugFix---修复oh-my-zsh安装闪退问题

--- init.sh
+++ init.sh
@@ -11,10 +11,8 @@ echo " ========================================================= "
 echo " |            Linux init.sh 环境部署脚本 V 1.1           | "
 echo " ========================================================= "
 echo "                      author：weikunkun                    "
-echo "                 https://github.com/Winniekun              "
+echo "         https://github.com/Winniekun/my-config-files      "
 echo -e "\n"
-
-# 由于学艺不精，经常把自己的服务器搞崩，并且定位不到root case
 # 所以经常会重装系统，痛定思痛，决定写一个一劳永逸的初始化安装配置脚本
 # LEVEL=base/dev/hacker/full
 #       base: 基础配置（zsh、vim、git、wget、curl等）
@@ -27,22 +25,17 @@ CUR_PATH=$(pwd)
 
 # zsh & oh-my-zsh安装
 config_zsh() {
-    if command -v zsh >/dev/null 2>&1
-    then
         echo -e "检测到zsh 已安装"
-    else 
+    else
         apt install -y zsh >/dev/null 2>&1
-    fi	
-    # install_ohmyzsh
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # sh -c "$(wget -O- https://pastebin.com/raw/FAFw08G6)"  
-    echo -e "shell切换为zsh"
-    chsh -s $(which zsh)
+    fi
+
+    install_ohmyzsh
+
     # 设置主题，新增插件
-    sed -i 's@ZSH_THEME="robbyrussell"@ZSH_THEME="awesomepanda"@g' ~/.zshrc 
-    sed -i 's@plugins=(.*)@plugins=(git extract zsh-syntax-highlighting autojump zsh-autosuggestions)@g' ~/.zshrc
+    sed -i 's@ZSH_THEME="robbyrussell"@ZSH_THEME="awesomepanda"@g' ~/.zshrc
     {
-        # 使用bat替代cat 
+        # 使用bat替代cat
         echo 'alias cat="/usr/bin/batcat"'
         echo 'alias myip="curl ifconfig.io/ip"'
         echo 'alias c=clear'
@@ -53,7 +46,6 @@ config_zsh() {
         echo -e "zsh-highlighting 已经安装"
     else
         echo -e "下载安装zsh-highlighting"
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     fi
 
     if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]
@@ -61,15 +53,15 @@ config_zsh() {
         echo -e "zsh-autosuggestions 已经安装"
     else
         echo -e "下载安装zsh-autosuggestions"
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     fi
-
+    sed -i '/export TERM=xterm-256color/d' ~/.zshrc
+    sed -i '/ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE/d' ~/.zshrc
     # 声明终端类型
-    echo "export TERM=xterm-256color" >> ~/.zshrc	
+    echo "export TERM=xterm-256color" >> ~/.zshrc
     # 设置建议命令的颜色
-    echo ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'" >> ~/.zshrc 	 
+    echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'" >> ~/.zshrc
     # 重载配置
-    source ~/.zshrc 
+    source ~/.zshrc
 }
 
 # oh-my-zsh安装
@@ -77,24 +69,23 @@ install_ohmyzsh() {
     echo -e "开始配置oh-my-zsh"
     if [ -d "$HOME/.oh-my-zsh/" ]
     then
-        test
+        rm -r $HOME/.oh-my-zsh
     fi
-    if [ -e "install.sh" ] 
+    if [ -e "install.sh" ]
     then
         rm install.sh
-    fi	
-    
-     wget -O install.sh https://pastebin.com/raw/FAFw08G6           
-    sed -i 's@REPO=${REPO:-ohmyzsh/ohmyzsh}@REPO=${REPO:-mirrors/oh-my-zsh}@g' install.sh
-    sed -i 's@REMOTE=${REMOTE:-https://github.com/${REPO}.git}@REMOTE=${REMOTE:-https://gitee.com/${REPO}.git}@g' install.sh
-    
+    fi
+    # 修改为国内镜像
+    wget wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
+
     sh install.sh
+    cd $CUR_PATH
 }
 
 # Vim 安装 & 配置
 install_vim() {
-    if [ ! -d "$CUR_PATH/PowerVim" ] 
-    then	    
+    if [ ! -d "$CUR_PATH/PowerVim" ]
+    then
         git clone https://github.com/youngyangyang04/PowerVim.git
     else
         VIM_PATH=$CUR_PATH/PowerVim
@@ -104,6 +95,7 @@ install_vim() {
     fi
     cd $CUR_PATH
 }
+
 # PowerVim的一些小问题修复
 fix_powervim() {
     # 系统中添加中文包
@@ -112,8 +104,8 @@ fix_powervim() {
     if [ ! -e "$LOCAL_FILE" ]
     then
         echo -e "创建 $LOCAL_FILE"
-        touch $LOCAL_FILE	
-    else 
+        touch $LOCAL_FILE
+    else
         rm $LOCAL_FILE
     fi
     {
@@ -122,27 +114,32 @@ fix_powervim() {
         echo "zh_CN.GBK GBK"
         echo "zh_CN GB2312"
     } >> $LOCAL_FILE
+    # 重新生成系统语言
+    sudo locale-gen
+    # 删除重复内容
+    sed -i '/let Tlist_Show_One_File=1/d' ~/.vimrc
+    sed -i '/let Tlist_Exit_OnlyWindow=1/d' ~/.vimrc
+    sed -i '/let Tlist_Ctags_Cmd=/d' ~/.vimrc
 
-sudo locale-gen
 
-{
+    {
     echo 'let Tlist_Show_One_File=1 "不同时显示多个文件的tag，只显示当前文件的'
     echo 'let Tlist_Exit_OnlyWindow=1 "如果taglist窗口是最后一个窗口，则退出vim'
     echo 'let Tlist_Ctags_Cmd="/usr/bin/ctags" "将taglist与ctags关联'
-} >> ~/.vimrc
+    } >> ~/.vimrc
 
-echo -e "修复成功"
+    echo -e "修复成功"
 }
 
 # Git 安装 & 配置
 intall_git() {
     if command -v git >/dev/null 2>&1
-    then 
+    then
         # 配置
         echo -e "检测到已经安装Git，开始Git配置"
         config_git
     else
-        # 安装 
+        # 安装
         apt install -y git >/dev/null 2>&1
         # 配置
         echo -e "开始Git配置"
@@ -153,9 +150,7 @@ intall_git() {
 config_git() {
     # 用户信息
     git config --global user.name "weikunkun"
-    git config --global user.email "kongwiki@163.com"
-    # core
-    git config --global core.editor vim   
+    git config --global core.editor vim
     # color
     git config --global color.ui true
     git config --global color.status "auto"
@@ -164,13 +159,12 @@ config_git() {
     git config --global merge.tool "vimdiff"
     # alias
     git config --global alias.co "checkout"
-    git config --global alias.br "branch"  
+    git config --global alias.br "branch"
     git config --global alias.ci "commit"
     git config --global alias.st "status"
     git config --global alias.last "log -1 HEAD"
 }
 
-# python3 配置
 install_python3() {
     if [ -d "$HOME/.pip" ]
     then
@@ -180,11 +174,8 @@ install_python3() {
     fi
     echo -e "[global]\n" >~/.pip/pip.conf
     # 替换PIP源 速度更快
-    echo -e "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >>~/.pip/pip.conf
     echo -e "开始安装Python常见库"
     pip3 install lxml >/dev/null 2>&1
-    pip3 install ipaddress >/dev/null 2>&1
-    pip3 install python-dateutil >/dev/null 2>&1
     pip3 install apscheduler >/dev/null 2>&1
     pip3 install mycli >/dev/null 2>&1
     pip3 install aiohttp >/dev/null 2>&1
@@ -201,39 +192,39 @@ install_python3() {
 # Java 配置
 install_java() {
     echo -e "开始配置Java环境"
-    if command -v java >/dev/null 2>&1 
+    if command -v java >/dev/null 2>&1
     then
         # 已经配置，不做操作
         echo -e "Java环境已经配置，即将跳过"
-        test 
-    else 
+        test
+    else
         echo -e "手动配置Java环境"
         if [ ! -e "/opt/jdk-11" ]
         then
-            wget https://repo.huaweicloud.com/java/jdk/11+28/jdk-11_linux-x64_bin.tar.gz
             tar -xzvf jdk-11_linux-x64_bin.tar.gz -C /opt >/dev/null 2>&1
         fi
         sed -i '/export JAVA_HOME=\/opt\/jdk-11/d' /etc/zsh/zprofile
         sed -i '/export PATH=${JAVA_HOME}\/bin:$PATH' /etc/zsh/zprofile
-        echo "export JAVA_HOME=/opt/jdk-11" >> /etc/zsh/zprofile 
-        echo "export PATH=${JAVA_HOME}/bin:$PATH" >> /etc/zsh/zprofile 
+        echo 'export JAVA_HOME=/opt/jdk-11' >> /etc/zsh/zprofile
+        echo 'export PATH=${JAVA_HOME}/bin:$PATH' >> /etc/zsh/zprofile
         source /etc/zsh/zprofile
         echo -e "Java环境配置完成"
     fi
-}
-
 # Go安装与配置
 install_go() {
-    if [ ! -e "/usr/local/go" ]; then
-        echo -e "Go文件还未下载，开始下载"
-        wget https://go.dev/dl/go1.17.3.linux-amd64.tar.gz
-        tar -zxvf go1.17.3.linux-amd64.tar.gz -C /usr/local
-    fi
-    echo -e "开始配置Go环境"
-    sed -i '/export PATH=\/usr\/local\/go\/bin:$PATH/d' /etc/zsh/zprofile
-    echo 'export PATH=/usr/local/go/bin:$PATH' >> /etc/zsh/zprofile
-    echo -e "刷新配置文件"
-    source /etc/zsh/zprofile
+    if command -v go >/dev/null 2>&1
+    then
+        echo -e "检测到已经安装Go 将跳过Go的安装"
+    else
+        if [ ! -e "/usr/local/go" ]; then
+                tar -zxvf go1.17.3.linux-amd64.tar.gz -C /usr/local
+        fi
+        echo -e "开始配置Go环境"
+        sed -i '/export PATH=\/usr\/local\/go\/bin:$PATH/d' /etc/zsh/zprofile
+        echo 'export PATH=/usr/local/go/bin:$PATH' >> /etc/zsh/zprofile
+        echo -e "刷新配置文件"
+        source /etc/zsh/zprofile
+     fi
 }
 
 # 系统配置
@@ -241,16 +232,14 @@ base_config() {
     echo -e "apt install ag ..."
     apt install -y silversearcher-ag >/dev/null 2>&1
     echo -e "apt install zh-hans 语言库"
-    apt install -y language-pack-zh-hans >/dev/null 2>&1 	
-    sudo apt-get install fonts-droid-fallback ttf-wqy-zenhei ttf-wqy-microhei fonts-arphic-ukai fonts-arphic-uming >/dev/null 2>&1
+    apt install -y language-pack-zh-hans >/dev/null 2>&1
     ulimit -n 10240
     echo -e "开始配置随机SSH 端口"
     SSH_PORT=$(cat /etc/ssh/sshd_config | ag -o '(?<=Port )\d+')
-    if [ $SSH_PORT -eq 22 ] 
+    if [ $SSH_PORT -eq 22 ]
     then
         SSH_NEW_PORT=$(shuf -i 10000-30000 -n1)
         echo -e "SSHD Port: ${SSH_NEW_PORT}" | tee -a ssh_port.txt
-        sed -E -i "s/(Port|#\sPort|#Port)\s.{1,5}$/Port ${SSH_NEW_PORT}/g" /etc/ssh/sshd_config
     fi
 
     # apt更新
@@ -274,11 +263,7 @@ base_config() {
         "which vim"
         "which sqlite3"
         "which lrzsz"
-        "which unzip"
-        "which git"
         "which curl"
-        "which wget"
-        "which bat"
         "which autojump"
     )
     for prog in "${cmdline[@]}"; do
@@ -301,9 +286,7 @@ base_config() {
     cd $CUR_PATH
     config_zsh
     echo -e "正在配置curl"
-    #curl https://raw.githubusercontent.com/al0ne/vim-for-server/master/.curlrc >~/.curlrc >/dev/null 2>&1
     echo -e "正在配置wget"
-    #curl https://raw.githubusercontent.com/al0ne/vim-for-server/master/.wgetrc >~/.wgetrc >/dev/null 2>&1
 
 }
 
@@ -349,38 +332,26 @@ dev_config() {
 
     install_java
     install_python3
+    install_go
 
     if command -v docker >/dev/null 2>&1
     then
         echo "检测到已经安装docker 将跳过Docker的安装"
-    else 
+    else
         echo -e "开始安装Docker"
         curl -fsSL https://get.docker.com -o get-docker.sh
-    fi
-
-    # Go 安装和配置
-    if command -v go >/dev/null 2>&1
-    then
-        echo -e "检测到已经安装Go 将跳过Go的安装"
-    else 
-        echo -e "开始安装Go"
-        sh -c "$(wget -O- https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh)"
     fi
 
     # Nodejs 配置
     if command -v node >/dev/null 2>&1; then
         echo -e "检测到已安装Nodejs 将跳过！"
-    else
-        echo -e "开始安装Nodejs"
         curl -fsSL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
         apt install -y nodejs >/dev/null 2>&1
-    fi 
+    fi
     # TODO Redis、MySQL、Kafka
 }
 
 # 网安配置
-hacker_config() {
-    echo -e "正在安装 nmap ..."
     apt install -y nmap >/dev/null 2>&1
     echo -e "正在安装 netcat ..."
     apt install -y netcat >/dev/null 2>&1
@@ -388,46 +359,33 @@ hacker_config() {
     apt install -y masscan >/dev/null 2>&1
     echo -e "正在安装 zmap ..."
     apt install -y zmap >/dev/null 2>&1
-    echo -e "正在安装 dnsutils ..."
-    apt install -y dnsutils >/dev/null 2>&1
     echo -e "正在安装 wfuzz ..."
     pip install wfuzz >/dev/null 2>&1
     # 安装 weevely3
     if [ -d "/opt/weevely3" ]; then
         echo -e "检测到weevely3已安装将跳过"
     else
-        echo -e "正在克隆 weevely3 ..."
-        cd /opt && git clone https://github.com/epinna/weevely3.git >/dev/null 2>&1
         echo 'alias weevely=/opt/weevely3/weevely.py' >>~/.zshrc
     fi
     # 安装 whatweb
     if [ -d "/opt/whatweb" ]; then
         echo -e "检测到whatweb已安装将跳过"
-    else
-        echo -e "正在克隆 whatweb ..."
-        cd /opt && git clone https://github.com/urbanadventurer/WhatWeb.git >/dev/null 2>&1
         echo 'alias whatweb=/opt/WhatWeb/whatweb' >>~/.zshrc
     fi
     # 安装 OneForAll
     if [ -d "/opt/OneForAll" ]; then
         echo -e "检测到OneForAll已安装将跳过"
-    else
-        echo -e "正在克隆 OneForAll ..."
-        cd /opt && git clone https://github.com/shmilylty/OneForAll.git >/dev/null 2>&1
     fi
     # 安装 dirsearch
     if [ -d "/opt/dirsearch" ]; then
         echo -e "检测到dirsearch已安装将跳过"
     else
-        echo -e "正在克隆 dirsearch ..."
-        cd /opt && git clone https://github.com/shmilylty/OneForAll.git >/dev/null 2>&1
     fi
     # 安装 httpx
     if command -v httpx >/dev/null 2>&1; then
         echo -e "检测到已安装httpx 将跳过！"
     else
         echo -e "开始安装Httpx"
-        GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx >/dev/null 2>&1
 
     fi
     # 安装 subfinder
@@ -435,7 +393,6 @@ hacker_config() {
         echo -e "检测到已安装subfinder 将跳过！"
     else
         echo -e "开始安装subfinder"
-        GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder >/dev/null 2>&1
 
     fi
     # 安装 nuclei
@@ -443,7 +400,6 @@ hacker_config() {
         echo -e "检测到已安装nuclei 将跳过！"
     else
         echo -e "开始安装nuclei"
-        GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei >/dev/null 2>&1
 
     fi
     # 安装 naabu
@@ -451,7 +407,6 @@ hacker_config() {
         echo -e "检测到已安装 naabu 将跳过！"
     else
         echo -e "开始安装 naabu"
-        GO111MODULE=on go get -u -v github.com/projectdiscovery/naabu/v2/cmd/naabu >/dev/null 2>&1
 
     fi
     # 安装 dnsx
@@ -459,7 +414,6 @@ hacker_config() {
         echo -e "检测到已安装 dnsx 将跳过！"
     else
         echo -e "开始安装 dnsx"
-        GO111MODULE=on go get -u -v github.com/projectdiscovery/dnsx/cmd/dnsx >/dev/null 2>&1
 
     fi
     # 安装 subjack
@@ -487,10 +441,10 @@ then
     echo "请切换到root用户再执行该脚本"
 fi
 
-if [ $LEVEL = 'base' ] 
+if [ $LEVEL = 'base' ]
 then
     base_config
-    chsh -s /bin/zsh 
+    chsh -s /bin/zsh
 fi
 
 if [ $LEVEL = 'dev' ]
@@ -500,7 +454,7 @@ then
     chsh -s /bin/zsh
 fi
 
-if [ $LEVEL = 'hacker' ] 
+if [ $LEVEL = 'hacker' ]
 then
     hacker_config
 fi
